b1d6d8ebd68b5a042b5afa1cfac70e48354d1299
Mastani23 <dhakshinadi.23bce9650@vitapstudent.ac.in>
2026-04-13 12:04:42 +0530

    Added  live url in backend

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ await connectdb();
 connectcloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173','https://xyz-five-lyart.vercel.app'];
 
 
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
